896f9123a6702d8b1f06079904264fa820edb866
MaheshKafle <mahesh.kafle@lisec.com>
2025-04-23 06:59:52 +0400

    Some comments

--- src/JavaOOP1/JavaFunctions.java
+++ src/JavaOOP1/JavaFunctions.java
@@ -2,9 +2,10 @@ package JavaOOP1;
 
 public class JavaFunctions 
 {
-
-	public static void main(String[] args) 
+	//main() -> execution starts from here
+	public static void main(String[] args)  //main method is void--never returns any value
 	{
+		//after creating an object , copy of all non static methods and variables is given to that objects.
 		JavaFunctions obj = new JavaFunctions();
 		obj.Test();
 		System.out.println(obj.QAtest());
@@ -12,11 +13,14 @@ public class JavaFunctions
 		System.out.println(obj.division(100,10));
 	}
 	
-	public void Test() //no input no output
+	//non-static methods
+	//void->doesn't return any value
+	public void Test() //no input no output 
 	{
 		System.out.println("Inside Test Method");
 	}
 
+	//return type-> int
 	public int QAtest() //no input some output
 	{
 		int a = 10;
@@ -25,12 +29,14 @@ public class JavaFunctions
 		return d;
 	}
 	
+	//return type -> String
 	public String Quote() // no input some output
 	{
 		String txt= "Java is awesome!";
 		return txt;
 	}
 	
+	//return type -> int
 	public int division(int a, int b) //some input some output
 	{
 		int d = a / b;
